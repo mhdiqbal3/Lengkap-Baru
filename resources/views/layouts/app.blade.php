7f8e538fc5310cd2a8e679d9d29a2d210341e830
mhdiqbal3 <mhdiqbal3@gmail.com>
2026-05-20 09:36:53 +0800

    Baru4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -325,18 +325,6 @@
                             d="M4 6h16M4 12h16M4 18h16"></path>
                     </svg>
                 </button>
-
-                <div class="relative w-full max-w-md hidden md:block">
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg>
-                    </div>
-                    <input type="text"
-                        class="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-[#800000] focus:border-[#800000] block w-full pl-10 p-2.5 outline-none transition"
-                        placeholder="Cari...">
-                </div>
             </div>
 
             <div class="flex items-center gap-4 relative">
